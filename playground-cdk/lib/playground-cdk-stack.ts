79f0d4800f68c8b7874420276bc81e385834a2c7
yamatatsu <the.kinnikuman@gmail.com>
2022-06-30 19:54:43 +0900

    slide 進捗

--- playground-cdk/lib/playground-cdk-stack.ts
+++ playground-cdk/lib/playground-cdk-stack.ts
@@ -1,11 +1,12 @@
-import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
+import * as cdk from "aws-cdk-lib";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as rds from "aws-cdk-lib/aws-rds";
 import * as assets from "aws-cdk-lib/aws-ecr-assets";
 import * as apprunner from "@aws-cdk/aws-apprunner-alpha";
 
-export class PlaygroundCdkStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class PlaygroundCdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const vpc = new ec2.Vpc(this, "Vpc", {
@@ -17,10 +18,28 @@ export class PlaygroundCdkStack extends Stack {
       ],
     });
 
+    const database = new rds.DatabaseCluster(this, "Database", {
+      engine: rds.DatabaseClusterEngine.auroraMysql({
+        version: rds.AuroraMysqlEngineVersion.VER_2_10_2,
+      }),
+      instanceProps: {
+        instanceType: ec2.InstanceType.of(
+          ec2.InstanceClass.T3,
+          ec2.InstanceSize.SMALL
+        ),
+        vpc,
+        vpcSubnets: { subnetGroupName: "db-subnet" },
+      },
+      defaultDatabaseName: "mydb",
+      credentials: rds.Credentials.fromGeneratedSecret("admin"),
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
     const vpcConnector = new apprunner.VpcConnector(this, "VpcConnector", {
       vpc,
       vpcSubnets: { subnetGroupName: "app-subnet" },
     });
+    database.connections.allowDefaultPortFrom(vpcConnector);
 
     const asset = new assets.DockerImageAsset(this, "ImageAssets", {
       directory: "./app",
@@ -32,6 +51,23 @@ export class PlaygroundCdkStack extends Stack {
         asset: asset,
         imageConfiguration: {
           port: 3000,
+          environment: {
+            DB_USERNAME: database
+              .secret!.secretValueFromJson("username")
+              .unsafeUnwrap(),
+            DB_PASSWORD: database
+              .secret!.secretValueFromJson("password")
+              .unsafeUnwrap(),
+            DB_HOST: database
+              .secret!.secretValueFromJson("host")
+              .unsafeUnwrap(),
+            DB_PORT: database
+              .secret!.secretValueFromJson("port")
+              .unsafeUnwrap(),
+            DB_NAME: database
+              .secret!.secretValueFromJson("dbname")
+              .unsafeUnwrap(),
+          },
         },
       }),
       vpcConnector,
